7e35fd5ce23b9609f1184f86a35481a6a84a4399
Herculano Lima <hercullanolima@gmail.com>
2023-08-01 15:58:33 -0300

    Novo teste de commit

--- primeiro-progrma-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-progrma-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -4,6 +4,9 @@ public class PrimeiraClasseJava {
 	
 	public static void main(String[] args) {
 		System.out.println("Primeiro programa Java!");
+		System.out.println("Primeiro programa Java!");
+		
+		/*COLOCANDO INFORMAÇÃO PARAhghghghgh COMMITAR*/
 	}
 
 }
